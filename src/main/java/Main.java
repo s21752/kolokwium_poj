9dd4927ebb6dccc9d3952704b8924c72d89e4d27
cezary.graban <cezary.graban@skyrise.tech>
2020-05-30 12:35:34 +0200

    Finished account number validation implementation via ValidationHelper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import utils.validation.ValidationHelper;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello Kolokwium!!!");
+
+        validateAccountNumber();
+    }
+
+    private static void validateAccountNumber() {
+        System.out.println(ValidationHelper.getInstance().isAccountNumberValid("11111111111111111111111190"));
     }
 }
